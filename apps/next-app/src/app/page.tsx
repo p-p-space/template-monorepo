ab1366dc24f2334167eb9305385bf42f272f03f6
Jesús Enrique Peñaloza Piñero <enrique.penaloza.working@gmail.com>
2026-08-28 09:15:38 -0400

    feat: add counter button  and update globe icon color in SVG for improved interactivity

--- apps/next-app/src/app/page.tsx
+++ apps/next-app/src/app/page.tsx
@@ -1,9 +1,14 @@
+'use client';
+
 import Image from 'next/image';
+import { useState } from 'react';
 
 import { Button } from '@space/ui/components/button';
 import { type UserInput, userSchema } from '@space/utils';
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+
   const parsed = userSchema.safeParse({
     name: 'Ana',
     email: 'ana@test.com',
@@ -44,19 +49,24 @@ export default function Home() {
             </a>{' '}
             center.
           </p>
+          <Button
+            className="flex h-12 w-full self-center items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-39.5"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            <Image className="dark:invert h-5 w-7" src="/globe.svg" alt="Globe icon" width={16} height={14} />
+            Count is {count}
+          </Button>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <Button className="rounded-full">
-            <a
-              className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-39.5"
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image className="dark:invert h-3.5 w-4" src="/vercel.svg" alt="Vercel logomark" width={16} height={14} />
-              Deploy Now
-            </a>
-          </Button>
+          <a
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-39.5"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image className="dark:invert h-3.5 w-4" src="/vercel.svg" alt="Vercel logomark" width={16} height={14} />
+            Deploy Now
+          </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-39.5"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
